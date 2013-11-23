64400ee86498612481a0d23114f9e6344343828f
Alex Hornung <alex@alexhornung.com>
2013-11-23 07:11:30 +0000

    remove crap from the render test

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 define(['jade!test.jade'], function(render_test) {
   console.log("render_test:");
-  console.log(render_test({names: ["Alex", "Gema"], bye: function(name) { return "Bye " + name + "!"; }}));
-  console.log(render_test({names: ["Pete", "Myka"], bye: function(name) { return "You suck, " + name + "!"; }}));
+  console.log(render_test({names: ["Pete", "Myka"], bye: function(name) { return "Bye " + name + "!"; }}));
 });
